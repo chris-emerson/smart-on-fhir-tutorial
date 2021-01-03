ef868c8a36355dc56b0979e0790df1caedf46c91
Chris Emerson <51479188+chris-emerson@users.noreply.github.com>
2021-01-03 18:20:29 +0000

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -8,6 +8,7 @@
     }
 
     function onReady(smart)  {
+      console.log(JSON.stringify(smart));
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
